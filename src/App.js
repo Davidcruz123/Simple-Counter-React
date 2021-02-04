8d8df1d3ff8274a97080d1459a6fd97a03d37bf1
David <daviducr.2593@gmail.com>
2021-02-04 16:32:42 -0600

    Mi primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// setInterval(function () {alert("Hello");},     1000      );
+
+
+
+
+export const Modulo1=(props) =>{
+    return (
+      <div className="cuadrado">
+        <h1>
+          
+          {props.value}
+          {props.icon}
+        </h1>
+        {/* <i class="far fa-clock"></i> */}
+      </div>
+    );
+
+
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,107 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Modulo1 } from './app.js'
+import './app.css'
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// var variable;
+
+// function myFunction() {
+//   variable = setInterval(ReactDOM.render, 1000);
+// }
+
+var segundos = 0;
+let minutosUnidades = 0;
+var minutosDecenas=0;
+var horaUnidad=0;
+var horaDecena=0;
+function Index() {
+  segundos++
+  let segundosString=segundos.toString()
+  let unidades=segundosString[segundosString.length-1]
+  let decenas=0;
+  if (segundos>9){
+    decenas = segundosString[segundosString.length - 2];
+  
+  if (segundos==60){decenas=0};
+  
+  }
+  
+
+if (segundos>59&&minutosUnidades==9){
+  minutosDecenas++
+  if (minutosDecenas==6){minutosDecenas=0
+  
+    horaUnidad++
+    if (horaUnidad==10){horaUnidad=0
+    
+      horaDecena++
+      
+      if(horaDecena==10){segundos=0
+      decenas=0
+    minutosDecenas=0
+  minutosUnidades=0
+horaUnidad=0
+horaDecena=0}
+
+
+    }
+  
+  
+  
+  
+  }
+}
+
+
+
+
+  if (segundos > 59) {
+    if (minutosUnidades<=9){
+      
+      
+      minutosUnidades++
+
+      if (minutosUnidades==10){minutosUnidades=0}
+
+    segundos = 0;
+    }else{minutosUnidades=0}
+    
+  }
+
+
+
+
+
+
+
+  return (
+    <div className="linea1">
+      <div></div>
+      <Modulo1 icon={<i class="far fa-clock"></i>} />
+      {/* Esto es como un componente, por eso va en {} */}
+      <Modulo1 value={horaDecena}/>
+      <Modulo1 value={horaUnidad}/>
+      <Modulo1 value={minutosDecenas}/>
+      <Modulo1 value={minutosUnidades}/>
+      <Modulo1 value={decenas}/>
+      <Modulo1 value={unidades} />
+    </div>
+  );
+
+
+}
+setInterval(function () {
+  ReactDOM.render(
+    <Index />,
+
+    document.getElementById("root")
+  );
+}, 1000
+
+)
+
+
+
+
